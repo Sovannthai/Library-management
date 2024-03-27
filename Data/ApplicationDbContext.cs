bbac038ce9a0c6f0a7dd54fce678b53ef988fe6f
Thai Sros <srosthai00@gmail.com>
2024-03-27 19:33:24 +0700

    Update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,5 +16,6 @@ namespace EmpManagement.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
     }
 }
